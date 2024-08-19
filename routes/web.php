d5461eb8606dbff6e24339455ed9e96f3f9c37ae
chegg-son <chegg.6964@gmail.com>
2024-08-19 17:19:36 +0700

    proses penambahan auth dan role

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,18 @@
 use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', [KategoriController::class, 'index'])->name('user');
-
 Route::get('/login-admin', function () {
     return view('pages.admin.login');
-});
+})->name('login');
+
+Route::post('actionlogin', [KategoriController::class, 'login'])->name('action.login');
+Route::get('actionlogout', [KategoriController::class, 'logout'])->name('action.logout');
 
 Route::get('/admin-panel', function () {
     return view('pages.admin.index');
-})->name('admin');
+})->name('admin.panel');
+
+Route::get('/', [KategoriController::class, 'index'])->name('user')->middleware('auth');
 
 Route::get('/checkout', function () {
     return view('pages.user.checkout');
