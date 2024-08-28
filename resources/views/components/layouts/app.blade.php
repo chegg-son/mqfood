22eae8fe9656d7237aff5f9619447d46a5b946aa
chegg-son <chegg.6964@gmail.com>
2024-08-28 18:00:09 +0700

    proses penambahan livewire

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,25 @@
+@extends('app')
+@section('content')
+    <div class="content-page">
+        <div class="content">
+            <div class="container-fluid">
+                <h1>{{ $slot }}</h1>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    <script></script>
+    <!-- Vendor -->
+    <script src="{{ asset('assets/libs/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/waypoints/lib/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/jquery.counterup/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
+
+    <!-- App js -->
+    <script src="{{ asset('assets/js/app.min.js') }}"></script>
+@endpush
