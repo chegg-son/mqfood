8f25f9fef04c04bea88e59abccdadc773a5f7f92
chegg-son <chegg.6964@gmail.com>
2024-09-25 10:07:21 +0700

    minor fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <title>PIAT7</title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
     <meta content="Coderthemes" name="author" />
